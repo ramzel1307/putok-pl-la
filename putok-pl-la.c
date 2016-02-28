ebfa4bbf8a7f6add6447105e215177cef8195e4b
ramzel1307 <lotoramzelrenz@gmail.com>
2016-02-28 23:09:58 +0800

    Uploaded initial c source code for lexical analyzer
    No code yet, just comments and possible functions we will use

---
+++ putok-pl-la.c
@@ -0,0 +1,30 @@
+/*	Program Title: 	PUTOK Programming Language Lexical Analyzer
+ *					Group Project
+ *	Group members:	Calvadores, Angelica
+                    Enrique, Mariella
+                    Loto, Ramzel Renz L.
+                    Roman, Rose Anne L.
+ *	Course/Sec/Sch:	BSCS 2-1N Polytechnic University of the Philippines - Mabini Campus
+ *	Date Written: 	February 1, 2016
+ *	Prepared for: 	Prof. Elias Austria
+ *					COSC 3053 - Principles of Programming Languages
+ *======================================================================================================
+ *
+ * This program is a Lexical Analyzer based on our proposed programming language "PUTOK".
+ *
+ */
+
+#include<stdio.h>
+#define MAX_NAME_SIZE 256
+
+/*===========MAIN FUNCTION==========*/
+int main(void){
+
+
+
+	return 0;
+}
+
+void lexical(){
+
+}
